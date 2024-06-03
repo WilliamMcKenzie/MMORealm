d082bfb85667e19dc63082eb297cc6cc17cbab92
Maxwell-Adams <maxwelladams00724@gmail.com>
2024-06-03 12:58:25 -0700

    crap that i gotta push to pull

--- Client/Scenes/SupportScenes/Objects/DungeonPortals/enter_dungeon.gd
+++ Client/Scenes/SupportScenes/Objects/DungeonPortals/enter_dungeon.gd
@@ -5,12 +5,13 @@ var object_id
 func _ready():
 	$Area2D.connect("area_entered", self, "OnPortal")
 	$Area2D.connect("area_exited", self, "OffPortal")
-
+	
 #Interface Section
 func OnPortal(body):
 	if body.get_parent().has_method("DefinePlayerState"):
 		GameUI.get_node("EnterPortal").visible = true
 		GameUI.get_node("EnterPortal").portal_id = object_id
+
 func OffPortal(body):
 	if body.get_parent().has_method("DefinePlayerState"):
 		GameUI.get_node("EnterPortal").visible = false
