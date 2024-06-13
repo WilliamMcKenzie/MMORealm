2e8078b8c280786f7bb78d5922696bc52f3ecbad
William <williamqmckenzie@gmail.com>
2024-06-13 09:45:55 -0300

    ui changes

--- Client/Scenes/MainScenes/island_container.gd
+++ Client/Scenes/MainScenes/island_container.gd
@@ -105,6 +105,7 @@ func UpdateWorldState(world_state):
 
 #Enemy nodes
 func SpawnNewEnemy(enemy_id, enemy_position, enemy_name):
+	print(enemy_position)
 	if not get_node("YSort/Enemies").has_node(str(enemy_id)):
 		var enemy_scene = load("res://Scenes/SupportScenes/Npcs/"+enemy_name+".tscn")
 		var enemy_instance = enemy_scene.instance()
